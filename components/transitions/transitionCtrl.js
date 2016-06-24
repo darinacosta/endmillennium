28eb7b3f05023ec617103db7766bf4cc078b2e1b
Darin <darinjacosta@gmail.com>
2016-06-23 20:55:03 -0500

    add styles

--- components/transitions/transitionCtrl.js
+++ components/transitions/transitionCtrl.js
@@ -7,17 +7,24 @@ function transitionCtrl() {
     transitionCtrl.activateTransitionEvents();
   };
 
-  var chaptersClicked = {
-    one: false,
-    two: false
+  var chapters = {
+    1: {
+      'title': 'The Highway',
+      'clicked': false,
+      'passage-name': 'The highway'
+    },
+    2: {
+      title: 'The Endless Aisles',
+      clicked: false
+    }
   };
 
   transitionCtrl.activateTransitionEvents = function(){
       $("html").on('click', function(event){
-        if ($(event.target).is('[passage-name="The highway"]') && !chaptersClicked.one) {
-          chaptersClicked.one = true;
+        if ($(event.target).is('[passage-name="The highway"]') && !chapters[1].clicked) {
+          chapters[1].clicked = true;
           $('tw-passage').fadeOut(0, function(){
-            $('body').append('<div class="chapter text-section" id="chapterone"><div class="inner-chapter"><div class="chapter-header">Chapter One</div><br><div class="chapter-title">The Highway</div></div></div>');
+            $('body').append('<div class="text-section chapter" id="chapterone"><div class="inner-chapter"><div class="chapter-header">Chapter One</div><br><div class="chapter-title">The Highway</div></div></div>');
             $('#chapterone').css('visibility','visible').hide().fadeIn('slow').delay(3000).fadeOut('slow', function(){
               $('tw-passage').fadeIn(1000);
             });
